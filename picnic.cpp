7ba2274691c4746103ac468d20f55d5c2ac6c1b0
[Kritika <kritikarag.kr@gmail.com>
2022-08-12 20:41:03 +0530

    modified:   picnic.cpp

--- picnic.cpp
+++ picnic.cpp
@@ -1,2 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+int main(){
+    int n,i,x,y,sum=0;
+    cin>>n;
+    map<int,int>mp;
+    vector<int>a(n,0);
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        mp[a[i]]++;
+    }
+
+    for (auto &it:mp){
+        ans+= (it.second/2);
+    }
+
+    cout<<ans<<endl;
+}
