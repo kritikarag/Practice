feef11723ea8efa03de863c3f094fcfa345b63b2
[Kritika <kritikarag.kr@gmail.com>
2022-08-16 08:20:37 +0530

    modified:   moving_apples.cpp

--- moving_apples.cpp
+++ moving_apples.cpp
@@ -6,24 +6,19 @@ int main()
 {
 
     int n;
-
     cin >> n;
-
     vector<int> a(n);
 
     for (auto &i : a)
     {
-
         cin >> i;
     }
 
     int avg = accumulate(a.begin(), a.end(), 0) / n;
-
     int ans = 0;
 
     for (auto i : a)
     {
-
         ans += abs(avg - i);
     }
 
