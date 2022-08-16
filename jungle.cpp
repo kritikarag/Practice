04d0fbc7d6d210859f7f599e9d69ad788ea7dc7b
[Kritika <kritikarag.kr@gmail.com>
2022-08-16 08:40:46 +0530

    modified:   jungle.cpp

--- jungle.cpp
+++ jungle.cpp
@@ -1,65 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int collatzLenUtil(int n, unordered_map<int, int> &collLenMap)
-{
-
-    // If value already
-    // computed, return it
-    if (collLenMap.find(n) != collLenMap.end())
-        return collLenMap[n];
-
-    // Base case
+int collatzLenUtil(int n, unordered_map<int, int> &mp)
+{ 
+    if (mp.find(n) != mp.end())
+        return mp[n];
+ 
     if (n == 1)
-        collLenMap[n] = 1;
-
-    // Even case
+        mp[n] = 1;
+ 
     else if (n % 2 == 0)
     {
-        collLenMap[n] = 1 + collatzLenUtil(n / 2, collLenMap);
+        mp[n] = 1 + collatzLenUtil(n / 2, mp);
     }
-
-    // Odd case
+ 
     else
     {
-        collLenMap[n] = 1 + collatzLenUtil(3 * n + 1, collLenMap);
+        mp[n] = 1 + collatzLenUtil(3 * n + 1, mp);
     }
 
-    return collLenMap[n];
+    return mp[n];
 }
 
 pair<int, int> collatzLen(int n)
 {
+ 
+    unordered_map<int, int> mp;
 
-    // Declare empty Map / Dict
-    // to store collatz lengths
-    unordered_map<int, int> collLenMap;
-
-    collatzLenUtil(n, collLenMap);
-
-    // Initialise ans and
-    // its collatz length
+    collatzLenUtil(n, mp);
+ 
     int num = -1, l = 0;
 
     for (int i = 1; i < n; i++)
-    {
+    { 
+        if (mp.find(i) == mp.end())
+            collatzLenUtil(i, mp);
 
-        // If value not already computed,
-        // pass Dict to Helper function
-        // and calculate and store value
-        if (collLenMap.find(i) == collLenMap.end())
-            collatzLenUtil(i, collLenMap);
-
-        int cLen = collLenMap[i];
+        int cLen = mp[i];
         if (l < cLen)
         {
             l = cLen;
             num = i;
         }
-    }
-
-    // Return ans and
-    // its collatz length
+    } 
     return {num, l};
 }
 
