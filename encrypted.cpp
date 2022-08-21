e2f3ed1228170a29371a7e36c01ade4c7502ca37
[Kritika <kritikarag.kr@gmail.com>
2022-08-22 00:32:57 +0530

    modified:   encrypted.cpp

--- encrypted.cpp
+++ encrypted.cpp
@@ -4,16 +4,16 @@ using namespace std;
 string getEncryptionNumber(int a[], int n)
 {
     // n-2th Pascal triangle line
-    int prev = 1, t = n - 2;
-    int f = a[0], e = a[n - 1];
-    for (int i = 1; i <= t; i++)
+    int prev = 1, total = n - 2;
+    int start = a[0], end = a[n - 1];
+    for (int i = 1; i <= total; i++)
     {
-        int curr = (prev * (t - i + 1)) / i;
+        int curr = (prev * (total - i + 1)) / i;
         prev = curr;
-        f += (a[i] * curr);
-        e += (a[n - i - 1] * curr);
+        start += (a[i] * curr);
+        end += (a[n - i - 1] * curr);
     }
-    return to_string(f % 10) + to_string(e % 10);
+    return to_string(start % 10) + to_string(end % 10);
 }
 
 int main(){
