64ff480855bb005ded8ef3e3baa22c16eba13215
Kritika Rag <kritikarag.kr@gmail.com>
2022-11-28 00:11:44 +0530

    modified:   modified_knapsack.cpp

--- earn_max_money.cpp
+++ earn_max_money.cpp
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-// int dp[101][101];
 
 int maxMergingScore(vector<int>&A)
 {
     A.insert(A.begin(),1);
     A.push_back(1);
     int N = A.size();
-    vector<vector<int>> dp(N, vector<int>(N));
+    vector<vector<int>> dp(N, vector<int>(N,0));
     for (int len = 1; len < N; ++len)
     {
         for (int i = 0; i + len < N; ++i)
         {
             int j = i + len;
-            dp[i][j] = 0;
+            //dp[i][j] = 0;
             for (int k = i + 1; k < j; ++k)
             {
                 dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + A[i] * A[k] * A[j]);
@@ -31,9 +30,6 @@ int main()
     for(int i=0;i<N;i++){
         cin>>A[i];
     } 
-    // int N = 6;
-    // int A[] = {1, 3, 1, 5, 8, 1};
     cout << maxMergingScore(A) << endl;
-
     return 0;
 }
--- modified_knapsack.cpp
+++ modified_knapsack.cpp
@@ -1,39 +1,90 @@
 #include<bits/stdc++.h>
 using namespace std;
+struct Elements
+{
+    int weight, value;
+    Elements(int x,int y)
+    { 
+        weight = x;
+        value = y;
+    }
+}; 
 
-int calculate(vector<vector<int>>&item){
-    int n = item.size();
-    vector<vector<int>>dp(n,vector<int>(W+1,0));
+int main(){
+    int n,W;
+    cin>>n>>W;
+    vector<Elements*>item[n+1];
+    for(int i=0;i<n;i++){
+        int t,w,v; 
+        cin>>t>>w>>v;
+        Elements *temp = new Elements(w,v); 
+        item[t].push_back(temp);
+    } 
+    vector<vector<int>> DP(n, vector<int>(W + 1, 0));
     for (int i = 0; i <= W; i++)
     {
         for (int j = 0; j < item[0].size(); j++)
         {
-            if (i >= item[0][j].weight)
+            if (i >= item[0][j]->weight)
             {
-                DP[0][i] = max(DP[0][i], item[0][j].value);
+                DP[0][i] = max(DP[0][i], item[0][j]->value);
             }
-        }
-    }
-
-    // Actual DP
+        } 
+    }  
 
     for (int k = 0; k <= W; k++)
     {
-
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i < n; i++)
         {
-
             for (int j = 0; j < item[i].size(); j++)
             {
-                if (k >= item[i][j].weight)
+                if (k >= item[i][j]->weight)
                 {
-                    DP[i][k] = max(DP[i][k], DP[i - 1][k - item[i][j].weight] + item[i][j].value);
+                    DP[i][k] = max(DP[i][k], DP[i - 1][k - item[i][j]->weight] + item[i][j]->value);
                 }
             }
 
-            DP[i][k] = max(DP[i][k], DP[i - 1][k]);
-        }
+            DP[i][k] = max(DP[i][k], DP[i - 1][k]); 
+        } 
     }
-
-    return dp[n-1][W];
+    cout<< DP[n - 1][W];
+    return 0;
 }
+
+// int calculate(vector<vector<int>> &item)
+// {
+//     int n = item.size();
+//     vector<vector<int>> dp(n, vector<int>(W + 1, 0));
+//     for (int i = 0; i <= W; i++)
+//     {
+//         for (int j = 0; j < item[0].size(); j++)
+//         {
+//             if (i >= item[0][j].weight)
+//             {
+//                 DP[0][i] = max(DP[0][i], item[0][j].value);
+//             }
+//         }
+//     }
+
+//     // Actual DP
+
+//     for (int k = 0; k <= W; k++)
+//     {
+
+//         for (int i = 0; i < n; i++)
+//         {
+
+//             for (int j = 0; j < item[i].size(); j++)
+//             {
+//                 if (k >= item[i][j].weight)
+//                 {
+//                     DP[i][k] = max(DP[i][k], DP[i - 1][k - item[i][j].weight] + item[i][j].value);
+//                 }
+//             }
+
+//             DP[i][k] = max(DP[i][k], DP[i - 1][k]);
+//         }
+//     }
+
+//     return dp[n - 1][W];
+// }
